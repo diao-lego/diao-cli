e558b83b99a3c85ca787313e763bf51a2310f881
ChenZhiPeng <634323580@qq.com>
2021-01-15 10:23:00 +0800

    使用yargs

---
+++ packages/core/bin/index.js
@@ -0,0 +1,66 @@
+#! /usr/bin/env node
+const dedent = require("dedent");
+const yargs  = require('yargs/yargs')
+// const { hideBin } = require('yargs/helpers')
+const pkg = require("../package.json");
+
+const context = {
+  diaoVersion: pkg.version,
+};
+
+const cli = yargs()
+const argv = process.argv.slice(2)
+cli
+  .usage('Usage: diao-cli [command] <options>')
+  .demandCommand(1, "A command is required. Pass --help to see all available commands and options.")
+  .strict()
+  .recommendCommands()
+  .fail((err, msg) => {
+    console.error('err:', err)
+  })
+  .alias('h', 'help')
+  .alias('v', 'version')
+  .wrap(cli.terminalWidth())
+  .epilogue(dedent`
+    When a command fails, all logs are written to lerna-debug.log in the current working directory.
+
+    For more information, find our manual at https://github.com/lerna/lerna
+  `)
+  .options({
+    debug: {
+      type: 'boolean',
+      describe: 'Bootstrap debug mode',
+      alias: 'd'
+    }
+  })
+  .option("registry", {
+    type: "string",
+    describe: 'Define global registry',
+    alias: 'r'
+  })
+  .group(['debug'], 'Dev Options')
+  .group(['registry'], 'Extra Options')
+  .command('init [name]', 'Do init a project', (yargs) => {
+    yargs
+      .option('name', {
+        type: 'string',
+        describe: 'Name of a project',
+        alias: 'n'
+      })
+  }, (argv) => {
+    console.log(argv)
+  })
+  .command({
+    command: 'list',
+    aliases: ['ls', 'la', 'll'],
+    describe: 'List local packages',
+    builder: (yargs) => {},
+    handler: (argv) => {
+      console.log(argv)
+    }
+  })
+  .parse(argv, context)
+
+// const utils = require('@diao-cli/utils')
+// console.log('hello diao-cli 屌爆了')
+// utils()
